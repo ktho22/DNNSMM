f0f4a7cd133cc9c8877b0162a1c0739080cb26be
ktho22 <ktho22@kaist.ac.kr>
2014-11-07 10:31:22 +0900

    length mismatched

--- datasets/make_timit_label.py
+++ datasets/make_timit_label.py
@@ -4,29 +4,49 @@ from os import listdir
 from os.path import isfile, isdir, join, splitext, basename
 import numpy as np
 
+datadir = '/dataset/kaldi/data-fmllr-tri3'
+savedir = '/dataset/kaldi/data-fmllr-tri3-edited'
 timitdir = '/dataset/timit/TIMIT'
 
 def make_timit_label(which_set, winsize, shift):
     datadir = join(timitdir,which_set)
     subdirs = [x[0] for x in os.walk(datadir) if x[1]==[]]
-    subdirs.sort(key=natural_keys)
+    alilist, phnlen, phnids = aliparser(which_set)
 
-    
+    uttlist=[]
     for sub in subdirs:
-        uttlist = [x for x in listdir(sub) if x.endswith('PHN')]
-        uttlist.sort(key=natural_keys)
-        spkname = basename(sub)
-        for utt in uttlist:
-            uttid = spkname+'_'+splitext(utt)[0]
-            uttpath = join(sub,utt)
-            with open(uttpath,'r') as fid:
-                phns = fid.read().split('\n')
-            phnlst60 = make_phonelist(phns,winsize,shift)
-            phnlst48 = phone60to48(phnlst60)
-            phnlst = phone48toidx(phnlst48,'pre')
-            phnlst = (str(x) for x in phnlst)
-            phnstr = uttid + ' ' + ' '.join(phnlst)
+        uttlist.extend([join(sub,x) for x in listdir(sub) if x.endswith('PHN')])
+    
+   # wid = open(join(datadir,'ali_mono_true'+which_set),'w')
+    wid = open('lengthinfo','w')
 
+    for ali in [alilist[8]]:
+    #for ali in alilist:
+        spk, utt = ali.split('_') 
+        uttpath =filter(lambda x:spk in x and utt in x, uttlist)[0]
+        with open(uttpath,'r') as fid:
+            phns = fid.read().split('\n')
+        phnlst60 = make_phonelist(phns,winsize,shift)
+        phnlst48 = phone60to48(phnlst60)
+        phnlst = phone48toidx(phnlst48,'pre')
+
+        ind = alilist.index(ali)
+        
+        numpad =  phnlen[ind] -len(phnlst) 
+        #phnlst.append(phnlst[-1]*numpad)
+        
+        length_ori = phnlen[ind]
+        length = len(phnlst)
+        ipdb.set_trace()
+        print ali, length_ori, length, length_ori-length
+        wid.write(' '.join((ali,str(length_ori),str(length),str(numpad)))+'\n')
+
+        #print [[x,y] for x,y in zip(phnids[ind], phnlst)]
+
+        phnlst_ = (str(x) for x in phnlst)
+        phnstr = ali + ' ' + ' '.join(phnlst_) + '\n'
+        #wid.write(phnstr)
+    wid.close()
 
 def make_phonelist(phns,winsize,shift,type='cut'):
     phnlst= []
@@ -42,8 +62,11 @@ def make_phonelist(phns,winsize,shift,type='cut'):
         remaining = phnlength - truelength + (winsize - shift)
         phnlst.extend([phn[2]]*numphn)
     
-    if nframestolength(len(phnlst),winsize,shift) and type == 'cut':
+    if nframestolength(len(phnlst),winsize,shift)>phns[-2].split(' ')[1]\
+        and type == 'cut':
         phnlst = phnlst[:-1]
+    
+
     return phnlst
 
 def nframestolength(nframes, winsize, shift):
@@ -53,7 +76,10 @@ def nframes(tot_len, winsize, shift, type='floor'):
     if type == 'floor':
         return 1+np.floor((tot_len-winsize)/shift)
     elif type == 'round':
-        return 1+np.round((tot_len-winsize)/shift)
+        n_in = 1+np.floor((tot_len-winsize)/shift)
+        l_res = tot_len-nframestolength(n_in,winsize,shift)
+        n_res = max(0,np.floor((2*l_res-winsize)/(2*shift)))
+        return n_in+n_res
     else: 
         return 1+np.ceil((tot_len-winsize)/shift)
 
@@ -61,6 +87,7 @@ def phone60to48(phone60):
     mapfile = '/home/thkim/libs/kaldi/egs/timit/s5/conf/phones.60-48-39.map'
     with open(mapfile,'r') as fid:
         prephmap = fid.read().split('\n')
+
     phmap = []
     for item in prephmap:
         if item == '':
@@ -106,6 +133,29 @@ def phone48toidx(phone48,q='amb'):
 def mapper(map, item, c1=0, c2=1):
     idx = np.where(map[c1,:]==item)
     return map[c2,idx[0]][0]
+
+def aliparser(which_set,nmodel='mono'):
+    keyword = 'ali_' + nmodel
+
+    # Obtain file name
+    alifiles = [ f for f in listdir(datadir) \
+        if isfile(join(datadir,f)) and f.startswith(keyword) and which_set.lower() in f]
+    alifiles.sort(key=natural_keys)
+
+    uttname = []
+    phnids = []
+    phnlen = []
+    for fname in alifiles:
+        rid = open(join(datadir,fname),'r')
+        line = rid.readline()
+        while not line in [[], '']:
+            spline = line.split()
+            uttname.append(spline[0].strip())
+            phnids.append(map(int,spline[1:]))
+            phnlen.append(len(spline[1:]))
+            line = rid.readline()
+        rid.close()
+    return uttname, phnlen, phnids
 
 if __name__=='__main__':
     which_set = 'TRAIN'
